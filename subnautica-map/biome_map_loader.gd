72c33f3671e4ba2e14cf6ba48c26e576695f4f22
Jonny Collins <jonny.collins111@gmail.com>
2026-04-01 06:25:55 +0100

    begun to fill in biome data loading function

--- subnautica-map/biome_map_loader.gd
+++ subnautica-map/biome_map_loader.gd
@@ -1,4 +1,15 @@
 class_name BiomeLoader extends RefCounted
 
 static func load_biomes() -> Array[BiomeInfo]:
+	var biome_colours: Dictionary[Color, BiomeInfo] = {}
+	var biome_map: Image = preload("res://biome-map.png").get_image()
+	var map_width: int = biome_map.get_width()
+	var map_height: int = biome_map.get_height()
+	for x in map_width:
+		for y in map_height:
+			var pixel_value = biome_map.get_pixel(x,y)
+			if pixel_value in biome_colours:
+				pass
+			else:
+				biome_colours[pixel_value] = BiomeInfo.new()
 	return []
